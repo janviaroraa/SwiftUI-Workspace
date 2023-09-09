7476df60578284e65b2f6d0847e72da1d8c9f221
Janvi Arora <jjanvi.arora18@gmail.com>
2023-09-09 17:25:49 +0530

    dynamic loader updates

---
+++ SwiftUIWorkspace/DynamicLoader.swift
@@ -0,0 +1,45 @@
+//
+//  DynamicLoader.swift
+//  SwiftUIWorkspace
+//
+//  Created by Janvi Arora on 09/09/23.
+//
+
+import SwiftUI
+
+struct DynamicLoader: View {
+    
+    @State private var isLoading: Bool = false
+    @State private var color: Color = .pink
+    
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    var body: some View {
+        ZStack {
+            Color.black.ignoresSafeArea()
+            
+            Circle()
+                .trim(from: .zero, to: 0.6)
+                .stroke(LinearGradient(colors: [color, .black.opacity(0.1)], startPoint: .leading, endPoint: .trailing), style: StrokeStyle(lineWidth: 8, lineCap: .round))
+                .frame(width: 60, height: 60)
+                .rotationEffect(Angle(degrees: isLoading ? 360 : 0))
+                .animation(.linear(duration: 1).repeatForever(autoreverses: false), value: isLoading)
+        }
+        .onReceive(timer) { _ in
+            withAnimation(.easeOut) {
+                isLoading = true
+                color = Color(
+                    red: Double.random(in: 0...1),
+                    green: Double.random(in: 0...1),
+                    blue: Double.random(in: 0...1)
+                )
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        DynamicLoader()
+    }
+}
