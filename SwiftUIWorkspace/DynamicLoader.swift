020982a44c2c49ac685994608de8deec52373939
Janvi Arora <jjanvi.arora18@gmail.com>
2023-09-09 17:30:50 +0530

    comments

--- SwiftUIWorkspace/DynamicLoader.swift
+++ SwiftUIWorkspace/DynamicLoader.swift
@@ -43,3 +43,20 @@ struct ContentView_Previews: PreviewProvider {
         DynamicLoader()
     }
 }
+
+
+/**
+
+Timer.publish(every: 1, on: .main, in: .common):
+ 
+This is the creation of a Timer publisher using the Timer class in SwiftUI's Combine framework. Here's what each part of this section does:
+
+every: 1: This part specifies that the Timer should emit a value every 1 second (i.e., it will produce an event every second).
+
+on: .main: It specifies that the Timer should operate on the main thread. This is important because UI-related operations should typically be performed on the main thread to ensure a smooth user interface.
+
+in: .common: This part specifies that the Timer should be scheduled in the common run loop mode. The common mode allows the Timer to work while the user interacts with the app, preventing it from being blocked by other operations.
+
+.autoconnect(): This is a method applied to the Timer publisher. The autoconnect() method is used to automatically connect (or start) the Timer publisher when it is added to a Combine pipeline. In other words, you don't need to manually call .connect() to start the Timer; it will start emitting values as soon as it's part of a Combine chain.
+
+ **/
