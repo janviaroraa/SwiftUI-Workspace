d24181f948d63e589a4a70aa4cf51c9bf29f36a7
Janvi Arora <jjanvi.arora18@gmail.com>
2023-09-09 16:53:47 +0530

    basic configuration

---
+++ SwiftUIWorkspace/SwiftUIWorkspaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWorkspaceApp.swift
+//  SwiftUIWorkspace
+//
+//  Created by Janvi Arora on 09/09/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWorkspaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
